4a410708026ce23e36488b97c5efe8c4bb2e58c7
devchiks3 <devwebira13@gmail.com>
2026-03-26 15:35:25 +0500

    Add files via upload

---
+++ counter/script.js
@@ -0,0 +1,26 @@
+const increaseButtonE1 = document.querySelector('.counter__button--increase')
+const decreaseButtonE1 = document.querySelector('.counter__button--decrease')
+const resetButtonE1= document.querySelector(".counter__reset-button")
+const counterValueE1= document.querySelector(".counter__value")
+
+resetButtonE1.addEventListener("click",function(){
+    counterValueE1.textContent = 0;
+})
+decreaseButtonE1.addEventListener("click",function(){
+    const currentValue = counterValueE1.textContent;
+    const currentValueAsNumber =+ currentValue;
+    let newValue = currentValueAsNumber -1;
+    if (newValue < 0) {
+        newValue=0;
+    }
+    counterValueE1.textContent = newValue;
+})
+increaseButtonE1.addEventListener("click",function(){
+    const currentValue = counterValueE1.textContent;
+    const currentValueAsNumber =+ currentValue;
+    const newValue = currentValueAsNumber +1;
+     if (newValue >5) {
+        newValue=5;
+    }
+    counterValueE1.textContent = newValue;
+})
